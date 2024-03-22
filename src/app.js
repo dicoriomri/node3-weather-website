aa58d09ebfecfae82b50390ca6107699aff56c19
omri.dicori <omri.dicori@xmcyber.com>
2024-03-22 11:00:04 +0200

    added notifications

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
+const {notificationsRegister, notificationsSend} = require('./utils/notifications')
+
 const forecast = require('./utils/forecast')
 
 const app = express()
@@ -26,6 +28,30 @@ app.get('/weather', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     geocode((error, { dateTime, date, time } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+        return res.send({dateTime, date, time});
+    })
+})
+
+app.get('/notification/register', (req, res) => {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    notificationsRegister(req, (error, { dateTime, date, time } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+        return res.send({dateTime, date, time});
+    })
+})
+
+app.get('/notification/send', (req, res) => {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    notificationsSend(req, (error, { dateTime, date, time } = {}) => {
         if (error) {
             return res.send({ error })
         }
---
+++ src/utils/notifications.js
@@ -0,0 +1,47 @@
+const request = require('request')
+const { initializeApp } = require('firebase-admin');
+const {getMessaging} = require("firebase-admin/messaging");
+
+let admin = require("firebase-admin");
+
+let serviceAccount = require("../json/headz-app-firebase-adminsdk-ffml9-29700b66b3.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+});
+
+const notificationsRegister = (req, callback) => {
+    const topic = req.query.topic;
+    const fcm_server_key = 'AAAAJrVJhUU:APA91bH6INBHtOCS3YZ8_pqsgpsHauwGkboG3H74pxxlqPJ3LNOJrxyrc5M7y7GRi-RFWIo9NUfOX6Uu1fHLdAiZsF4BZNc4z0GYWEV78ZaS6Dl1tuxnbXNWe1SCvwdiZg0Ltv7GNDui';
+    const deviceToken = req.query.deviceToken;
+    console.log(topic, deviceToken)
+
+    // const url = 'https://iid.googleapis.com/iid/v1/'+ deviceToken +'/rel/topics/' + topic
+    getMessaging().subscribeToTopic(deviceToken, topic).then(((response) => {
+        console.log(response)
+        callback(response);
+    }));
+}
+
+const notificationsSend = (req, callback) => {
+    const topic = req.query.topic;
+    const fcm_server_key = 'AAAAJrVJhUU:APA91bH6INBHtOCS3YZ8_pqsgpsHauwGkboG3H74pxxlqPJ3LNOJrxyrc5M7y7GRi-RFWIo9NUfOX6Uu1fHLdAiZsF4BZNc4z0GYWEV78ZaS6Dl1tuxnbXNWe1SCvwdiZg0Ltv7GNDui';
+    const deviceToken = req.query.deviceToken;
+    console.log(topic, deviceToken)
+    const message = {
+        data: {
+            title: req.query.title,
+            body: req.query.title
+        },
+        topic: topic
+    };
+
+    // const url = 'https://iid.googleapis.com/iid/v1/'+ deviceToken +'/rel/topics/' + topic
+    getMessaging().send(message).then(((response) => {
+        console.log(response)
+        callback(response);
+    }));
+}
+
+module.exports = {notificationsRegister, notificationsSend}
