5627a8e98d18d70cb494b7a974a59c246120f7d4
omri.dicori <omri.dicori@xmcyber.com>
2024-03-23 23:58:28 +0200

    added notifications

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -38,6 +38,9 @@ const notificationsSend = (req, callback) => {
             //     link: 'https://headz-app.web.app/game/' + req.query.gameID
             // }
         },
+        notification: {
+            link: 'https://headz-app.web.app/game/' + req.query.gameID
+        },
         // fcmOptions: {
         //     link: 'https://headz-app.web.app/game/' + req.query.gameID
         // },
