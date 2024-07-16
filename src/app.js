74cc30324aa816f7618537c2f54810627c574400
omri.dicori <omri.dicori@xmcyber.com>
2024-07-16 20:52:21 +0300

    added more stats

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.get('/weather', (req, res) => {
 app.get('/version', (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    return res.send({androidVersion: "3.40", iosVersion: "3.27", version: "3.27"});
+    return res.send({androidVersion: "3.40", iosVersion: "3.40", version: "3.40"});
 
 })
 
