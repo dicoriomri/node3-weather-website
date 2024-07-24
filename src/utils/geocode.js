3796f9cde5724356cdb27975548d959d2088c94b
omri.dicori <omri.dicori@xmcyber.com>
2024-07-24 15:26:05 +0300

    added more staff

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -27,5 +27,34 @@ const getPlace = (req, callback) => {
     });
 
 }
+const getWeather = (req, callback) => {
+    const q = req.query.q;
+    const dt = req.query.dt;
+    const isFuture = req.query.isFuture;
+    const playHour = req.query.playHour;
+    const days = req.query.days;
+    // https://maps.googleapis.com/maps/api/place/details/json?placeid=++&language=he-IL&key=AIzaSyDIvZu54YTDopUSFw2fIp6XnMixHiIveCI
+    let url = ''
+    if (!isFuture) {
+        url = 'http://api.weatherapi.com/v1/future.json?key=12b7a45856014d2889661515242407&q='+ q +'&dt=' + dt
 
-module.exports = {geocode, getPlace}
+    } else {
+        url = 'http://api.weatherapi.com/v1/forecast.json?key=12b7a45856014d2889661515242407&q='+ q +'&days=' + days
+    }
+    console.log(url)
+
+    let options = {
+        'method': 'GET',
+        'url': url,
+        json: true
+
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+
+        callback(null, response);
+    });
+
+}
+
+module.exports = {geocode, getPlace, getWeather}
