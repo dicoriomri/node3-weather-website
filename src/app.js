3117631a62762b389408693e2aec245f5bb1172c
dicoriomri <59414973+dicoriomri@users.noreply.github.com>
2024-07-28 16:21:26 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ app.get('/getWeather', (req, res) => {
 app.get('/version', (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    return res.send({androidVersion: "4.02", iosVersion: "4.01", version: "4.02"});
+    return res.send({androidVersion: "4.02", iosVersion: "4.02", version: "4.02"});
 
 })
 
